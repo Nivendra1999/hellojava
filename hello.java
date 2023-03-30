1e25af039b91fdb595e3bff7db2824a58e5d3b72
Nivrndrra <nivendravishvakarma@gmail.com>
2023-03-30 16:37:08 +0530

    new by me

--- hello.java
+++ hello.java
@@ -1,10 +1,6 @@
 class main{
     public static void main(String[] args) {
-        System.out.println("hello");
-        int a =7;
-        int b=9;
-        int c=a+b;
-        System.out.println(c);
+    System.out.println("hdgsdyui';lkjhgfdsddgdgdyfdjhfjyfyjfyjfdhy");
         
     }
     
